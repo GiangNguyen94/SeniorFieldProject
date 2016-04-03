6a75babf6c014ef7e47d667d22aed6aca48b7fe6
qxx <charlesqx@gmail.com>
2016-04-02 22:15:59 -0400

    dump to sqlite

--- Github Language/json_to_sqlite.py
+++ Github Language/json_to_sqlite.py
@@ -1,43 +1,37 @@
 import sqlite3
 import json
+import re
 
-conn = sqlite3.connect('repos.sqlite')
+conn = sqlite3.connect('repos-sm.sqlite')
 c = conn.cursor()
 
-## repos_0, create table
-with open("repos/repos_0.json") as f:
-	js = f.read()
 
-repos = json.loads(js)
-current_keys = repos[0].keys()
 
-attrs = ",".join(map(lambda s: s+" text",current_keys))
+sql_key_list = ["name", "full_name", "owner", "languages_url", "issue_events_url", "forks_url", "commits_url"]
+attrs = ",".join(map(lambda s: s+" text",sql_key_list))
 c.execute("DROP TABLE IF EXISTS repos")
-c.execute("CREATE TABLE repos ({})".format(attrs)) # Possible SQL injection
-
-for repo in repos:
-	for key in repo.keys():
-		c.execute("INSERT INTO repos({}) VALUES(?)".format(key), (unicode(repo[key]),))
-	
-for repo in repos:
-	Insert id, id
-	for key in repo.keys():
-		if key not id:
-			Updeate id=id (key) (value)
+c.execute("CREATE TABLE repos ({})".format(attrs)) 
 
 
+## repos_0, create table
+def insert_data(start=0, end=49):
+	for idir in range(start, end):
+		dir_name="repos/part{}".format(idir)
+		for ifile in range(0,100):
+			file_number = idir*100+ifile
+			file_name = "repos_{}.json".format(file_number)
+			with open(dir_name+"/"+file_name) as f:
+				repos = json.loads(f.read())
+				for repo in repos:
+					c.execute("INSERT INTO repos VALUES(?,?,?,?,?,?,?)", \
+						(repo["name"], repo["full_name"], \
+							repo["owner"]["login"], \
+							repo["languages_url"], \
+							re.sub("{.*}", "", repo["issue_events_url"]), \
+							repo["forks_url"], re.sub("{.*}", "", repo["commits_url"])))
 
-for repo in repos:
-	Insert into repos(key,key,k,k,k,k....) values( values, values, vlaues,vvd....)
 
+
+insert_data(0,3)
 conn.commit()
-
-# ## repos_1-4899
-# for i in range(1,4900):
-# 	with open("repos/repos_{}.json".format(i)) as f:
-# 		js = f.read()
-# 	repos = json.loads(js)
-
-
-
 conn.close()
