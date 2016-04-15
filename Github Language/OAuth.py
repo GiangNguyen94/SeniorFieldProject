d028e1ba9aa4f5381b3d20b22111498f6e6cc5c9
qxx <charlesqx@gmail.com>
2016-04-15 16:07:01 -0400

    remove exposed tokens

--- Github Language/OAuth.py
+++ Github Language/OAuth.py
@@ -1,5 +1,14 @@
-TOKENS = ["c8e0df7a7c5e9b967e76e705d32883fccc60738f"]
+import sys
+
+with open('access_token.secret') as f:
+	TOKENS = [line.rstrip() for line in f]
+
+n_tokens = len(TOKENS)
+count = 0
 
 # returns a token
 def token():
-	return TOKENS[0]
+	global count
+	count = count%sys.maxint+1
+	i = count % n_tokens
+	return TOKENS[i]
--- Github Language/analysis/OAuth.py
+++ Github Language/analysis/OAuth.py
@@ -1,9 +1,9 @@
 import sys
 
-TOKENS = ["c8e0df7a7c5e9b967e76e705d32883fccc60738f", \
-			"c818af58573556e73e4d059f02fd347bf07e8dee"]
-n_tokens = len(TOKENS)
+with open('access_token.secret') as f:
+	TOKENS = [line.rstrip() for line in f]
 
+n_tokens = len(TOKENS)
 count = 0
 
 # returns a token
