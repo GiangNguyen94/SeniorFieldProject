d5642fd20e9e0dfa1c1a3bf177daa87d95372337
qxx <charlesqx@gmail.com>
2016-04-18 12:40:43 -0400

    getting the angular contributors contribution on other repos

--- Github Language/analysis/fun_facts.py
+++ Github Language/analysis/fun_facts.py
@@ -1,6 +1,6 @@
 import user_stats
 import history
-import repo_user
+# import repo_userr
 # stargazers
 
 # language q=language:xxx
@@ -9,7 +9,8 @@ import repo_user
 # user_stats.get_user_repo_counts(0,3)
 
 # 5 repos with most stars #include history
-history.find_live_by_search_query("stars:>=100", sort="stars", limit=15)
+# history.find_live_by_search_query("stars:>=100", sort="stars", limit=5)
+history.find_live_by_search_query("stars:>=100", sort="stars", limit=5, report_type=["issue_event"])
 
 # owners and contributers of the 5 repos with most stars
 # repo_user.find_live_by_search_query("stars:>=100", sort="stars", limit=5)
@@ -18,3 +19,6 @@ history.find_live_by_search_query("stars:>=100", sort="stars", limit=15)
 
 # the top 30 java repos with the most stars (search/repositories?q=language:Java&sort=stars)
 # history.find_live_by_search_query("language:Java", sort="stars", limit=30)
+
+# see the how the same group of contributors perform on other repos
+# repo_user.cross_plot_contributions_on_other_repos("users20160415022344","angular","angular.js")
--- Github Language/analysis/repo_user.py
+++ Github Language/analysis/repo_user.py
@@ -5,6 +5,7 @@ import OAuth
 import urllib
 import json
 import IPython
+import csv
 
 def create_report_folder():
 	folder_name = "reports/users{}".format(datetime.datetime.now().strftime("%Y%m%d%H%M%S"))
@@ -49,3 +50,38 @@ def find_live_by_search_query(q, limit=10, sort="", order="desc", report_type=["
 		report_owners(results["items"][:limit], folder_name)
 	if "contributors" in report_type:
 		report_contributors(results["items"][:limit], folder_name)
+
+def cross_plot_contributions_on_other_repos(folder, owner, repo):
+	with open("reports/{}/{}/{}.csv".format(folder,owner,repo)) as csvfile:
+		csvfile.readline()
+		line = str.rstrip(csvfile.readline())
+		topcontributor = str.split(line,',')[0]
+	url = "https://api.github.com/users/{}/repos?sort=updated&access_token={}".format(topcontributor,OAuth.token())
+	repos_json = urllib.urlopen(url).read()
+	repos = json.loads(repos_json)
+	ids = [item["id"] for item in repos]
+	result_list=[]
+	for r in repos:
+		repoid = r["id"]
+		url = "https://api.github.com/repositories/{}/contributors?access_token={}".format(repoid,OAuth.token())
+		contribution_json = urllib.urlopen(url).read() 
+		contribution = json.loads(contribution_json)
+		result_dic = {}
+		for u in contribution:
+			dic_key = "{}".format(u["login"])
+			result_dic[dic_key]=u["contributions"]
+		result_list.append(result_dic)
+	IPython.embed()
+	with open("reports/{}/{}/{}.cross.csv".format(folder,owner,repo), "w") as o:
+		with open("reports/{}/{}/{}.csv".format(folder,owner,repo)) as f:
+			f.readline()
+			for line in f.readlines():
+				newline = str.rstrip(line)
+				username = str.split(newline,",")[0]
+				for dic in result_list:
+					if username in dic:
+						newline += ",{}".format(dic[username])
+					else:
+						newline += ",0"
+				print >> o, newline
+
