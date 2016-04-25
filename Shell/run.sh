c27716d2befd4e0e0918ef5989cd6307d868ac1d
qxx <charlesqx@gmail.com>
2016-04-25 13:42:53 -0400

    shell skeleton

--- Shell/run.sh
+++ Shell/run.sh
@@ -1,6 +1,32 @@
+#!/bin/bash
 # ./run.sh [--most-popular]
+##### REPL
+function repl {
+	echo "this is repl"
+}
 
-# REPL
+##### most-popular
+function most_popluar {
+	echo "this is most popluar"
+}
 
+##### documentation
+function usage {
+	echo "this is usage"
+}
 
-# most-popular
+##### main
+if [ $# -gt 0 ]; then
+	case $1 in
+		--most-popular )	most_popluar
+							;;
+		-h | --help )		usage
+							exit
+							;;
+		* )					usage
+							exit 1
+	esac
+else
+	repl
+fi
+
